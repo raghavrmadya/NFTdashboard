5e82a0117559b12340a6b1e001bf9690eadf800f
Raghav <91579519+raghavrmadya@users.noreply.github.com>
2022-04-04 00:23:07 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x99aef5163bfff738d12a74cae406688b230ac7a9",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "NFT Idols",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://lh3.googleusercontent.com/cvE8-Mqn_81Eby7iiSfb7imVqtV6STi1HOsjSu5Nur13UdK9tOOaFLZ6B4DvA6rTSuQdrqCVy6t743-zjeDjxIbbGJyZjn7Rfn8X=w600",
   },
 
   // Supported networks - just for your reference
